845e7533d0ac4c7870dc807235c9ce7de5af4883
CzerwinskiDawid <120742243+CzerwinskiDawid@users.noreply.github.com>
2023-02-19 17:45:33 +0100

    test

--- src/main/java/com/course/selenium/Main.java
+++ src/main/java/com/course/selenium/Main.java
@@ -2,6 +2,6 @@ package com.course.selenium;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Test");
     }
 } 
